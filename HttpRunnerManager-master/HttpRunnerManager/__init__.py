0651e404f72d375798d15b20e97e36c3738114e2
lvziye00lvziye <13294028578@163.com>
2020-07-30 10:46:00 +0800

    第一次提交代码

---
+++ HttpRunnerManager-master/HttpRunnerManager/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, unicode_literals
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
+import pymysql
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
+
+__all__ = ['celery_app']
